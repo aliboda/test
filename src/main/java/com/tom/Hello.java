bfbe57ec9b53022a0f3046dee330dddc572d5b1c
aliboda <gto.aliboda@gmail.com>
2019-03-03 23:39:58 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//       System.out.print("Hello world");
+        //new Person().hello();
+        Person P =new Person();
+        P.hello();
+    }
+}
---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,11 @@
+package com.tom;
+
+public class Person {
+    //被新增出類的類別叫物件
+    //new Person()
+    public void hello(){
+        System.out.println("Hello world");
+    }
+
+}
+
